dbc00ffcc5c57dadef38c09da2e72aa76ae76d67
skiddedrat <145750113+skiddedrat@users.noreply.github.com>
2026-05-13 22:36:53 +0300

    Fix identifier namespace after Shine Client rename

--- src/main/kotlin/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -110,6 +110,7 @@ object LiquidBounce : EventListener {
      * WARNING: Please read the GNU General Public License
      */
     const val CLIENT_NAME = "Shine Client"
+    const val CLIENT_NAMESPACE = "shineclient"
     const val CLIENT_AUTHOR = "CCBlueX"
 
     private object Client : Config("Client") {
